79f5f887774aacfd5b5f2b54aa15bf4e87fc8db3
Asad-Kahloon <asadkahloon77@gmail.com>
2024-12-29 23:24:16 +0500

    Initial commit from Create Next App

---
+++ src/components/header/header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function header() {
+  return (
+    <>
+      <div className='w-full flex justify-between py-4 px-8 items-center'>
+        <p> Logo Here </p>
+        <p> Text Here </p>
+        <p> Sign up buttons here </p>
+      </div>
+      <div className='w-full flex justify-between py-4 px-8 items-center'>
+        <p>Link here</p>
+        <p>Link here</p>
+        <p>Link here</p>
+        <p>Link here</p>
+      </div>
+    </>
+  )
+}
+
+export default header
